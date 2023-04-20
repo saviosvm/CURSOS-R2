b1f209dd0c70765325bf41449ea5f1c711515adb
saviosvm <saviofg1@hotmail.com>
2023-04-20 20:10:10 -0300

    conversão de tipo no php 8

---
+++ curso-php8/conversao-de-Tipos.php
@@ -0,0 +1,19 @@
+<?php
+$valor = "3";
+
+$valor *= 2;
+$valor *= 2.3;
+$novoValor = (string) $valor;
+
+var_dump(get_debug_type($novoValor));
+
+var_dump($novoValor);
+
+settype($novoValor, "integer");
+
+var_dump($novoValor);
+
+
+var_dump(get_debug_type($novoValor));
+
+?>
