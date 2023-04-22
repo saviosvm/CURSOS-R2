5d0cd0b31a1a8e3c0c465a3ddc0201abd1c18fda
saviosvm <saviofg1@hotmail.com>
2023-04-22 09:38:23 -0300

    operadores logicos

---
+++ curso-php8/operadores-logicos.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 10;
+$b = 20;
+$c = 30;
+/*
+var_dump($a > $b && $a > $c);
+var_dump($a < $b || $a > $c);
+var_dump(!($a < $b)); //inverte o resultado false vira true e true false
+*/
+
+var_dump(true || true);
+var_dump(true xor false);  // só da verdadeiro se um dor falso e o outro verdadeiro
+                       
+
+?>
