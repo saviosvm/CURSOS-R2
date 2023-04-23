999d572283d7a8949a6bc596fd4f514399e66074
saviosvm <saviofg1@hotmail.com>
2023-04-23 17:24:26 -0300

    switch

---
+++ curso-php8/switch.php
@@ -0,0 +1,16 @@
+<?php
+$nome = "Figueiredo";
+
+switch($nome){
+    case "Savio":
+        echo "Estou estudando php novamente....";
+    break;
+    case "Figueiredo":
+        echo "Meu Sobrenome";
+    break;
+
+    default:
+    echo "...";
+
+}
+?>
