dc81b3ad433a98358e8edefde910c3b10aa451da
saviosvm <saviofg1@hotmail.com>
2023-05-01 16:53:14 -0300

    argc-argv

---
+++ curso-php8/argc-argv.php
@@ -0,0 +1,11 @@
+<?php
+
+var_dump($argc); // permite adicionar um argumento
+
+var_dump($argv); //mostra quais são os argumentos através de um array
+
+if ($argv[1] == "--savio"){
+    echo "Atualizando a liguagem PHP8";
+}
+
+?>
