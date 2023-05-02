40267ce24735c9c8cd655ecd7bdc6272b5840119
saviosvm <saviofg1@hotmail.com>
2023-05-02 19:54:40 -0300

    start e end

---
+++ curso-php8/start_with-wndwith.php
@@ -0,0 +1,10 @@
+<?php
+
+$msg = "Eng. Sávio Figueiredo";
+
+// var_dump(str_starts_with($msg, "Eng")); verifica se começa com a palavra
+// var_dump(str_ends_with($msg, "Figueiredo")); // verifica se termina com a palavra
+
+
+
+?>
