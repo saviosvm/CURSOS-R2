4cb520297b61cf11759fc60b6401bd4b6e8ddb9b
saviosvm <saviofg1@hotmail.com>
2023-05-03 18:05:06 -0300

    strpos-ipos

---
+++ curso-php8/strpos-stripos.php
@@ -0,0 +1,8 @@
+<?php
+
+$msg = "Sávio Figueiredo de Morais";
+
+//echo strpos($msg, "Figueiredo", 5); // mostra a posição da palavra, ou seja, onde ela começa
+
+echo stripos($msg, "figueiredo"); // a string não precisa ser exatamente igual
+?>
