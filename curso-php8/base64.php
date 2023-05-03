3f879c9abf6a7c028cc6c89747df864a1b38e3e3
saviosvm <saviofg1@hotmail.com>
2023-05-03 19:37:32 -0300

    base64

---
+++ curso-php8/base64.php
@@ -0,0 +1,9 @@
+<?php
+
+$msg = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum reprehenderit magnam, hic cum nam quo sint assumenda impedit sapiente quasi eaque veniam saepe exercitationem earum iure facere minima ad doloremque.";
+
+echo strlen($msg);
+
+echo base64_encode($msg);
+
+?>
