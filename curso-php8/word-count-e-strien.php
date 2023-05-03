f23681a98d9523c24a4ff43ca7884ae15bf12e92
saviosvm <saviofg1@hotmail.com>
2023-05-03 17:52:22 -0300

    strlen e word_count

---
+++ curso-php8/word-count-e-strien.php
@@ -0,0 +1,8 @@
+<?php
+
+$msg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.";
+
+//echo strlen($msg)." caracteres"; //conta caracteres
+echo str_word_count($msg)." palavras"; //conta palavras
+
+?>
