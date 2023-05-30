09755dcb5ff02f11f99c2e10038faf4db43613d6
saviosvm <saviofg1@hotmail.com>
2023-05-30 19:20:04 -0300

    store - products

---
+++ marketplace/database/migrations/2023_05_30_214207_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            
+            $table->string('nome');
+            $table->string('descricao');
+            $table->text('body');
+            $table->decimal('preco', 10, 2);
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
