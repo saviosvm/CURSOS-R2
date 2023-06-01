c284d6e3d6ab25cf2b109de5ad83c9f568e5971e
saviosvm <saviofg1@hotmail.com>
2023-06-01 19:06:09 -0300

    Eloquent Models

--- marketplace/routes/web.php
+++ marketplace/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/model', function(){
+    //$products = \App\Models\Product::all();
+    
+    
+   //$user = \App\Models\User::find(1);
+   // $user->name = 'Savio Morais';
+    //$user->save();
+
+   // return \App\Models\User::all();
+
+//return $user->where('id', '>', '5')->first();
+
+//dd(\App\Models\User::where('name', 'Thais')->get());;
+//$user = \App\Models\User::create([
+   // 'name' => 'Thais',
+    //'email' => 'thaisfg1@hotmail.com',
+   // 'password' => bcrypt('1234567895'),
+
+//]);
+
+//$user = \App\Models\User::find(9);
+//$user->update([
+  //  'name' => 'Atualizando os dados',
+//]);
+
+
+
+    
 });
